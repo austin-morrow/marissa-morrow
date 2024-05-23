0a8fa59042ac87aecac5f39defd5d4fada972b9d
Austin Morrow <austinmorrowdev@gmail.com>
2024-05-22 21:48:08 -0600

    contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,66 @@
 <template>
-    <div >
-      <h1>Contact page</h1>
+  <div
+    class="relative isolate overflow-hidden bg-[#F2F1EC] px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0"
+  >
+    <div class="grid gap-8 sm:grid-cols-2 sm:items-center">
+      <div class="flex justify-center sm:order-1">
+        <img
+          src="../assets/images/IMG_1845.JPG"
+          alt="Image"
+          class="w-full h-auto max-w-full lg:max-w-96 lg:h-auto"
+        />
+      </div>
+
+      <div class="flex justify-center sm:order-2">
+        <div style="width: 100%">
+          <iframe
+            ref="hecticForm"
+            allowtransparency="true"
+            style="border: 0; width: 100%; height: 800px"
+            :height="iframeHeight"
+          ></iframe>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <style>
-  </style>
-  
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    // Access the iframe element using $refs
+    const hecticFrame = this.$refs.hecticForm;
+
+    // Set the iframe source
+    hecticFrame.src =
+      "https://hello.withmoxie.com/01/morrow-manor-photography/marissa-website-form?inFrame=true&sourceUrl=" +
+      encodeURIComponent(window.location.href);
+
+    // Set up iframe resizing after it's loaded
+    hecticFrame.onload = () => {
+      this.resizeIframe();
+    };
+  },
+  methods: {
+    resizeIframe() {
+      // Resize the iframe using iframe-resizer
+      iFrameResize(
+        {
+          heightCalculationMethod: "taggedElement", // Use the height of the content inside the iframe
+          log: false,
+          checkOrigin: false,
+        },
+        this.$refs.hecticForm
+      );
+
+      // Optional: Adjust iframe height dynamically
+      const contentHeight =
+        this.$refs.hecticForm.contentWindow.document.body.scrollHeight;
+      this.iframeHeight = `${contentHeight}px`;
+    },
+  },
+};
+</script>
